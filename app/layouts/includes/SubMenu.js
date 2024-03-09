c7abcc425d158b5df9042e21e1800d7bf69968b1
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2024-03-09 07:04:38 +0100

    feat: remove extra menus in the main page

--- app/layouts/includes/SubMenu.js
+++ app/layouts/includes/SubMenu.js
@@ -6,14 +6,14 @@ export default function SubMenu() {
         { id: 1, name: 'Home' },
         { id: 2, name: 'Saved' },
         { id: 3, name: 'Electronics' },
-        { id: 4, name: 'Motors' },
-        { id: 5, name: 'Fashion' },
-        { id: 6, name: 'Collectables and Art' },
-        { id: 7, name: 'Sports' },
-        { id: 8, name: 'Health & Beauty' },
-        { id: 9, name: 'Industrial Equipment' },
-        { id: 10, name: 'Home & Garden' },
-        { id: 11, name: 'Sell' },
+        // { id: 4, name: 'Motors' },
+        // { id: 5, name: 'Fashion' },
+        // { id: 6, name: 'Collectables and Art' },
+        // { id: 7, name: 'Sports' },
+        // { id: 8, name: 'Health & Beauty' },
+        // { id: 9, name: 'Industrial Equipment' },
+        // { id: 10, name: 'Home & Garden' },
+        // { id: 11, name: 'Sell' },
     ]
 
     return (
