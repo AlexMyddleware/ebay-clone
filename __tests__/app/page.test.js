5c40020fd1bca048fa4401a6ab4f956a93376396
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2024-03-16 11:57:23 +0100

    feat: page test for home, babel config file

--- __tests__/app/page.test.js
+++ __tests__/app/page.test.js
@@ -1,5 +1,22 @@
-const addFunction = require('../app/functions.test.js');
+// Import necessary modules
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Product from '../../app/components/Product';
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(addFunction(1, 2)).toBe(3);
+test('renders Product component with given product', () => {
+    // Mock product
+    const product = {
+        id: 1,
+        title: 'Product 1',
+        price: 10000,
+        url: 'http://example.com/product1'
+    };
+
+    // Render Product component with mock product
+    render(<Product product={product} />);
+
+    // Check if product details are rendered
+    expect(screen.getByText(product.title)).toBeInTheDocument();
+    expect(screen.getByText(`USD ${(product.price / 100).toFixed(2)}`)).toBeInTheDocument();
+    expect(screen.getByRole('img')).toHaveAttribute('src', product.url + '/190');
 });
