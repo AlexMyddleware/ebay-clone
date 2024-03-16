fd1371ed217fd235a379a53120ebf9cafc15f7a4
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2024-03-16 11:39:35 +0100

    feat: first passing test

---
+++ __tests__/app/functions.test.js
@@ -0,0 +1,11 @@
+// This is your function in a file named `functions.js`
+function add(a, b) {
+    return a + b;
+}
+
+test('this is a simple test', () => {
+    expect(true).toBe(true);
+});
+
+
+module.exports = add;
