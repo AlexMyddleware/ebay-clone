2de133dd9a64f8e897a99f98329e1fe21581705a
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2024-04-01 18:13:55 +0200

    feat: tab component

--- app/components/Tabs.js
+++ app/components/Tabs.js
@@ -4,25 +4,35 @@
 import { useCart } from "../context/cart"
 import { toast } from "react-toastify"
 import "@/app/styles/Tabs.css";
-import React from "react";
+import React, { useState } from "react";
 
 const Tabs = () => {
+
+    const [toggleState, setToggleState] = useState(1);
+
+    const toggleTab = (index) => {
+        setToggleState(index);
+    }
+
     return (
         <div>
             <div className="container">
                 <div className="bloc-tabs">
-                    <button className="tabs active-tabs">
+                    <button className={toggleState === 1? 'tabs active-tabs' : 'tabs'} 
+                    onClick={() => toggleTab(1)}>
                         Education
                     </button>
-                    <button className="tabs">
-                        Experience
+                    <button className={toggleState === 2? 'tabs active-tabs' : 'tabs'} 
+                    onClick={() => toggleTab(2)}>
+                        Education
                     </button>
-                    <button className="tabs">
-                        Skills
+                    <button className={toggleState === 3? 'tabs active-tabs' : 'tabs'} 
+                    onClick={() => toggleTab(3)}>
+                        Education
                     </button>
                 </div>
                 <div className="content-tabs">
-                    <div className="content">
+                    <div className={setToggleState === 1 ? 'content active-content' : 'content'}>
                         <h2>Experience</h2>
                         <hr />
                         <li>Worked at Google</li>
@@ -31,7 +41,7 @@ const Tabs = () => {
                         <li>Worked at Microsoft</li>
                         <li>Worked at Netflix</li>
                     </div>
-                    <div className="content">
+                    <div className={setToggleState === 2 ? 'content active-content' : 'content'}>
                         <h2>Skills</h2>
                         <hr />
                         <li>HTML</li>
@@ -43,7 +53,7 @@ const Tabs = () => {
                         <li>Typescript</li>
                         <li>Express</li>
                     </div>
-                    <div className="content active-content">
+                    <div className={setToggleState === 3 ? 'content active-content' : 'content'}>
                         <h2>Education</h2>
                         <hr />
                         <li>Computer Science</li>
