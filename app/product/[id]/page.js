7a99bfececca0ea5c9d26182572ddb1fabaf324c
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2024-04-07 10:55:32 +0200

    remove fields from product page

--- app/product/[id]/page.js
+++ app/product/[id]/page.js
@@ -41,14 +41,9 @@ export default function Product({ params }) {
 
             <div className="px-4 w-full">
               <div className="font-bold text-xl">{product?.title}</div>
-              <div className="text-sm text-gray-700 pt-2">Brand New - Full Warranty</div>
-
               <div className="border-b py-1" />
 
               <div className="pt-3 pb-2">
-                <div className="flex items-center">
-                  Condition: <span className="font-bold text-[17px] ml-2">New</span>
-                </div>
               </div>
 
               <div className="border-b py-1" />
@@ -85,10 +80,6 @@ export default function Product({ params }) {
 
               <div className="border-b py-1" />
 
-              <div className="pt-3">
-                <div className="font-semibold pb-1">Description:</div>
-                <div className="text-sm">{product?.description}</div>
-              </div>
 
             </div>
           </div>
