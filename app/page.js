0b37e855d3e2ee936572c3a0ded79c11e03bbd3c
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2024-04-07 10:47:47 +0200

    feat: replace home page with regular page, will put list of product somewhere

--- app/page.js
+++ app/page.js
@@ -1,40 +1,16 @@
 "use client";
 
-import { useEffect, useState } from 'react';
-import Product from './components/Product';
 import MainLayout from './layouts/MainLayout';
-import useIsLoading from "@/app/hooks/useIsLoading"
 
 export default function Home() {
-
-  const [products, setProducts] = useState([])
-
-  const getProducts = async () => {
-    useIsLoading(true)
-
-    const response = await fetch('/api/products')
-    const prods = await response.json()
-
-    setProducts([])
-    setProducts(prods)
-    useIsLoading(false)
-  }
-
-  useEffect(() => { getProducts() }, [])
-
   return (
-    <>
-      <MainLayout>
-        <div className="max-w-[1200px] mx-auto">
-          <div className="text-2xl font-bold mt-4 mb-6 px-4 text-center">Our token</div>
-  
-          <div className="flex justify-center">
-            {products.map(product => (
-              <Product key={product.id} product={product} />
-            ))}
-          </div>
-        </div>
-      </MainLayout>
-    </>
+    <MainLayout>
+      <div className="max-w-[1200px] mx-auto">
+        <div className="text-2xl font-bold mt-4 mb-6 px-4 text-center">Welcome!</div>
+        <p className="mt-6 px-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+    </MainLayout>
   )
 }
